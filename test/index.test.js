b482a85c6a4ec4151a834977d48d1e29819a7cc7
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-02-13 07:48:43 +0100

    Test successful response with done

--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,7 @@ import sinon from 'sinon'
 
 describe('Testing async call that returns a Promise', function () {
 
-  it('should', function () {
+  it('should return success response (with done)', function (done) {
     // Ingredients:
     let async = {
       doSomenthingAsync: sinon.stub()
@@ -14,10 +14,15 @@ describe('Testing async call that returns a Promise', function () {
     })
 
     // Instructions:
-    subject.asyncCallWithPromise().then(function (response) {
-      expect(response).to.equal('Test success')
-    })
+    subject.asyncCallWithPromise()
+      .then(function (response) {
+        expect(response).to.equal('Test success')
+        done()
+      })
+      //Catch the AssertionError thrown if the expectation above is not met
+      .catch(function (err) {
+        done(err)
+      })
     async.doSomenthingAsync.invokeCallback(undefined, 'Test success')
-
   })
 })
