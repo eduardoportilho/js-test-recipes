5c8c55901fa6fca389be8a34e406f68e6975864a
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-02-13 07:49:32 +0100

    Test error response with done

--- test/index.test.js
+++ test/index.test.js
@@ -25,4 +25,26 @@ describe('Testing async call that returns a Promise', function () {
       })
     async.doSomenthingAsync.invokeCallback(undefined, 'Test success')
   })
+
+  it('should return error response (with done)', function (done) {
+    // Ingredients:
+    let async = {
+      doSomenthingAsync: sinon.stub()
+    }
+    let subject = proxyquire('../src/index', {
+      './async': async
+    })
+
+    // Instructions:
+    subject.asyncCallWithPromise()
+      .catch(function (err) {
+        expect(err).to.equal('Test error')
+        done()
+      })
+      //Catch the AssertionError thrown if the expectation above is not met
+      .catch(function (err) {
+        done(err)
+      })
+    async.doSomenthingAsync.invokeCallback('Test error x', undefined)
+  })
 })
